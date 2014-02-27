87a34c9c644ccf76cc197e9b72a0905453f9b78d
Brandon Huey <brandon@stylograph.net>
2014-02-26 23:11:10 -0800

    thing to fix hash values

--- src/workers/collect_station_metrics.rb
+++ src/workers/collect_station_metrics.rb
@@ -3,8 +3,30 @@ require 'rubygems'
 require 'buoy_data'
 require 'rethinkdb'
 include RethinkDB::Shortcuts
-require './util'
+#require './util'
 require 'json'
+
+def fixnumify(obj)
+    if obj.respond_to? :to_f
+        # If we can cast it to a Fixnum, do it.
+        obj.to_f
+
+    elsif obj.is_a? Array
+        # If it's an Array, use Enumerable#map to recursively call this method
+        # on each item.
+        obj.map {|item| fixnumify item }
+
+    elsif obj.is_a? Hash
+        # If it's a Hash, recursively call this method on each value.
+        obj.merge( obj ) {|k, val| fixnumify val }
+
+    else
+        # If for some reason we run into something else, just return
+        # it unmodified; alternatively you could throw an exception.
+        obj
+
+    end
+end
 
 stations = Array.new
 stations = [46232, 46225, 46231, 46242, 46224, 46086]
@@ -16,7 +38,7 @@ loop do
         report = station.get
         report['station_id'] = id
         report['timestamp'] = DateTime.now.strftime('%Q')
-        report = fixnumify report
+        report = fixnumify(report)
         report.delete("STEEPNESS")
         report.delete("WWD")
         report.delete("SwD")
